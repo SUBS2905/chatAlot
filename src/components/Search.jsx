25621cfa989c4bcd97df974c3c1bb4e702c538f3
Subhransu <subhranshumajhi2@gmail.com>
2022-12-12 15:45:13 +0530

    add search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className='searchForm'>
+        <input type='text' placeholder='search user'/>
+      </div>
+      <div className='userChat'>
+        <img src='https://tinyurl.com/user-saitama' alt='' />
+        <div className='userChatInfo'>
+          <span>Saitama</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
