74c22884d9a869466ad671f37610c73e7cdc9b7f
Subhransu <subhranshumajhi2@gmail.com>
2022-12-12 20:05:47 +0530

    chat area user info

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className='chatInfo'>
+        <span>Saitama</span>
+        <div className='chatIcons'>
+          <VideocamIcon />
+          <PersonAddIcon />
+          <MoreHorizIcon />
+        </div>
+      </div>
+    </div>
   )
 }
 
