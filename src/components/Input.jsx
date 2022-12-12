8bb28ccd8081301130ad785a8550f0f68a245817
Subhransu <subhranshumajhi2@gmail.com>
2022-12-12 21:09:13 +0530

    add message input area

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import SendIcon from '@mui/icons-material/Send';
+import { pink } from '@mui/material/colors';
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className='input'>
+      <input type='text' placeholder='Write your message...'/>
+      <div className='inputIcons'>
+        <AttachFileIcon />
+        <input type='file' id='addpic' style={{display: 'none'}} />
+        <label htmlFor='addpic'><AddPhotoAlternateIcon /></label>
+        <button><SendIcon sx={{color: pink[300]}}/></button>
+      </div>
+    </div>
   )
 }
 
