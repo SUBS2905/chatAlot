1f55a3f71351836933842d8e96e14d39ec99ef6f
Subhransu <subhranshumajhi2@gmail.com>
2022-12-12 15:50:24 +0530

    add user chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Chats = () => {
   return (
-    <div>Chats</div>
-  )
-}
+    <div className="chats">
+      <div className="userChat">
+        <img src="https://tinyurl.com/user-saitama" alt="" />
+        <div className="userChatInfo">
+          <span>Saitama</span>
+          <p>Recent message!</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Chats
+export default Chats;
