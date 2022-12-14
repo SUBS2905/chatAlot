9e38158b35695735fb471dafcad907e9b38ef858
Subhransu <subhranshumajhi2@gmail.com>
2022-12-14 21:22:32 +0530

    message styling

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div className='message'>
-        Message
+    <div className='message sender'>
+        <div className='messageInfo'>
+          <img src='https://tinyurl.com/user-saitama' alt=''/>
+          <span>Just now</span>
+        </div>
+        <div className='messageContent'>
+          <p>Hello there</p>
+          <img src='https://tinyurl.com/yorForger123' alt='' />
+        </div>
     </div>
   )
 }
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -10,6 +10,12 @@ const Messages = () => {
     <Message />
     <Message />
     <Message />
+    <Message />
+    <Message />
+    <Message />
+    <Message />
+    <Message />
+    <Message />
   </div>;
 };
 
