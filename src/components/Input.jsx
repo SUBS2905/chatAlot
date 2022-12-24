63b192941224d1501c52bf556e52ee68570bc194
Subhransu <subhranshumajhi2@gmail.com>
2022-12-24 15:32:49 +0530

    change message input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
-import SendIcon from '@mui/icons-material/Send';
-import { pink } from '@mui/material/colors';
+import React from "react";
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import SendIcon from "@mui/icons-material/Send";
+import { grey } from "@mui/material/colors";
 
 const Input = () => {
   return (
-    <div className='input'>
-      <input type='text' placeholder='Write your message...'/>
-      <div className='inputIcons'>
-        <AttachFileIcon />
-        <input type='file' id='addpic' style={{display: 'none'}} />
-        <label htmlFor='addpic'><AddPhotoAlternateIcon /></label>
-        <button><SendIcon sx={{color: pink[300]}}/></button>
+    <div className="input">
+      <div className="inputWrapper">
+        <input type="text" placeholder="Write your message..." />
+        <div className="inputIcons">
+          <AttachFileIcon style={{color: grey[300]}} />
+          <input type="file" id="addpic" style={{ display: "none" }} />
+          <label htmlFor="addpic">
+            <AddPhotoAlternateIcon style={{color: grey[300]}} />
+          </label>
+          <button>
+            <SendIcon style={{color: grey[300]}} />
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
