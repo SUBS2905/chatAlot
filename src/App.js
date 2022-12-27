a7f3e540fe3948698525979e4f9a8fad6e11642d
Subhransu <subhranshumajhi2@gmail.com>
2022-12-27 12:40:25 +0530

    routing and auth context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const {currentUser} = useContext(AuthContext);
+
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
