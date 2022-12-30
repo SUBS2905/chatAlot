8d86135c06950ff3e9a4860f8558f6ddb6047e14
Subhransu <subhranshumajhi2@gmail.com>
2022-12-30 13:36:19 +0530

    add login authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Login = () => {
-  return (
-    <div className='formContainer'>
-        <div className='formWrapper'>
-            <span className='logo'>Chat-a-Lot</span>
-            <span className='title'>Login</span>
-            <form>
-                <input type='email' placeholder='email' />
-                <input type='password' placeholder='password' />
-                <button>Sign In</button>
-            </form>
-            <p>Don't have an account? Register</p>
-        </div>
-    </div>
-  )
-}
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
 
-export default Login
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch {
+      setErr(true);
+    }
+  };
+
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chat-a-Lot</span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign In</button>
+          {err ? <span>Something went wrong!</span> : null}
+        </form>
+        <p>Don't have an account? <Link to="/register">Register</Link></p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
