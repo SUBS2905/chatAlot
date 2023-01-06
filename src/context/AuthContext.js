c46051c7392e3c4b98c1730ff150f4bf015fbc15
Subhransu <subhranshumajhi2@gmail.com>
2023-01-06 12:59:56 +0530

    minor update

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -9,7 +9,6 @@ export const AuthContextProvider = ({ children }) => {
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
-      console.log(user);
     });
 
     return () => {
