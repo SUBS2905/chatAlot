9bd5031a68e5f70a045001820657ae068b167ff1
Subhransu <subhranshumajhi2@gmail.com>
2023-01-09 13:49:27 +0530

    code refactor

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,5 @@
 import React, { useContext, useState } from "react";
-import {
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  query,
-  serverTimestamp,
-  setDoc,
-  updateDoc,
-  where,
-} from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, updateDoc, where} from "firebase/firestore";
 import { db } from "../firebase";
 import { AuthContext } from "../context/AuthContext";
 
