e10173f8fd6e3cc4ebd65c5f3fbc041a34257dd1
Subhransu <subhranshumajhi2@gmail.com>
2023-01-10 00:33:42 +0530

    add sent message to db

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,22 +1,75 @@
-import React from "react";
-import AddCircleIcon from '@mui/icons-material/AddCircle';
+import React, { useContext, useState } from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
 import SendIcon from "@mui/icons-material/Send";
 import { blue, grey } from "@mui/material/colors";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+import { arrayUnion, doc, Timestamp, updateDoc } from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { v4 as uuid } from "uuid";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 
 const Input = () => {
+  const [text, setText] = useState("");
+  const [img, setImg] = useState(null);
+
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  const handleSend = async () => {
+    if (img) {
+      const storageRef = ref(storage, uuid());
+      const uploadTask = uploadBytesResumable(storageRef, img);
+      const uploadSnapshot = await uploadTask;
+      const downloadURL = await getDownloadURL(uploadSnapshot.ref);
+
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+          img: downloadURL,
+        }),
+      });
+    } else {
+      await updateDoc(doc(db, "chats", data.chatId), {
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          senderId: currentUser.uid,
+          date: Timestamp.now(),
+        }),
+      });
+    }
+
+    setText("");
+    setImg(null);
+  };
   return (
     <div className="input">
       <div className="inputWrapper">
-        <input type="text" placeholder="Write your message..." />
+        <input
+          type="text"
+          placeholder="Write your message..."
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        />
+        <input
+          id="img-upload"
+          type="file"
+          style={{ display: "none" }}
+          onChange={(e) => setImg(e.target.files[0])}
+          value={img}
+        />
         <div className="inputIcons">
-          <AddCircleIcon style={{color: grey[300]}} />
-          <AddPhotoAlternateIcon style={{color: grey[300]}} />
-          {/* <input type="file" id="addpic" style={{ display: "none" }} />
-          <label htmlFor="addpic">
-          </label> */}
-          <button>
-            <SendIcon style={{color: blue[600]}} />
+          <AddCircleIcon style={{ color: grey[300] }} />
+          <label htmlFor="img-upload" className="img-upload-input">
+            <AddPhotoAlternateIcon style={{ color: grey[300] }} />
+          </label>
+          <button onClick={handleSend}>
+            <SendIcon style={{ color: blue[600] }} />
           </button>
         </div>
       </div>
