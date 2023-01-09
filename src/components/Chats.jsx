91e6868b7eeec773db00238f7087d4823c42d314
Subhransu <subhranshumajhi2@gmail.com>
2023-01-09 13:53:10 +0530

    feature: add user to chat list

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { db } from "../firebase";
 
 const Chats = () => {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+
+      return () => unsub();
+    };
+
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+  console.log(Object.entries(chats));
   return (
     <div className="chats">
-      <div className="userChat">
-        <img src="https://tinyurl.com/user-saitama" alt="" />
-        <div className="userChatInfo">
-          <span>Saitama</span>
-          <p>Recent message!</p>
+      {Object.entries(chats)?.map((chat) => (
+        <div className="userChat" key={chat[0]}>
+          <img src={chat[1].userInfo.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
