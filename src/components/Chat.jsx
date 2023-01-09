67e4266da3ab0685a3ac3a89f8d500e0d09e6ddd
Subhransu <subhranshumajhi2@gmail.com>
2023-01-09 21:53:47 +0530

    display user info in chat area

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import VideocamIcon from '@mui/icons-material/Videocam';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import LogoutIcon from '@mui/icons-material/Logout';
@@ -6,12 +6,16 @@ import Messages from './Messages';
 import Input from './Input'
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
+import { ChatContext } from '../context/ChatContext';
 
 const Chat = () => {
+
+  const {data} = useContext(ChatContext);
+
   return (
     <div className='chat'>
       <div className='chatInfo'>
-        <span>Saitama</span>
+        <span>{data.user?.displayName}</span>
         <div className='chatIcons'>
           <VideocamIcon />
           <PersonAddIcon />
