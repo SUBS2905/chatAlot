eb3fed3504f1270f8589cc04a1a4365e32dd3d01
Subhransu <subhranshumajhi2@gmail.com>
2023-01-10 13:10:54 +0530

    fetch messages from db and send to frontend

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,22 +1,28 @@
-import React from "react";
-import Message from "./Message"
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { ChatContext } from "../context/ChatContext";
+import { db } from "../firebase";
+import Message from "./Message";
 
 const Messages = () => {
-  return <div className="messages">
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-    <Message />
-  </div>;
+  const { data } = useContext(ChatContext);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => unsub();
+  }, [data.chatId]);
+
+  return (
+    <div className="messages">
+      {messages.map((m)=>(
+        <Message message={m} key={m.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Messages;
