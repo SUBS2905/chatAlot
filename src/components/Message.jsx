9660532bb5285b8c31ae66b9036cbdac5568e12e
Subhransu <subhranshumajhi2@gmail.com>
2023-01-10 13:26:34 +0530

    update message data from messages prop

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
+const Message = ({ message }) => {
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
 
-const Message = () => {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  }, [message]);
+
   return (
-    <div className='message sender'>
-        <div className='messageInfo'>
-          <img src='https://tinyurl.com/user-saitama' alt=''/>
-          <span>Just now</span>
-        </div>
-        <div className='messageContent'>
-          <p>Hello there</p>
-          <img src='https://tinyurl.com/yorForger123' alt='' />
-        </div>
+    <div
+      ref={ref}
+      className={`message ${message.senderId === currentUser.uid && "sender"}`}
+    >
+      <div className="messageInfo">
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""
+        />
+        <span>{message.date.toDate().toLocaleTimeString()}</span>
+      </div>
+      <div className="messageContent">
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt="" />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
