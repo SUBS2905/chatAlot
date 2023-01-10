2f84cb32688e3720f28456836c1cadd2ef934fe7
Subhransu <subhranshumajhi2@gmail.com>
2023-01-10 13:12:45 +0530

    remove minor bug

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -26,12 +26,12 @@ const Chats = () => {
   }
   return (
     <div className="chats">
-      {Object.entries(chats)?.map((chat) => (
+      {Object.entries(chats)?.sort((a,b)=>b[1].date - a[1].date).map((chat) => (
         <div className="userChat" key={chat[0]} onClick={()=>handleSelect(chat[1].userInfo)}>
           <img src={chat[1].userInfo.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{chat[1].userInfo.displayName}</span>
-            <p>{chat[1].userInfo.lastMessage?.text}</p>
+            <p>{chat[1].lastMessage?.text}</p>
           </div>
         </div>
       ))}
